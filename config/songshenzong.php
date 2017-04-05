77c8d60819b81c5d12516da0450727628b88b05c
connor <connor.niu@gmail.com>
2017-04-05 12:53:18 +0800

    fixed minor bugs

--- config/songshenzong.php
+++ config/songshenzong.php
@@ -26,7 +26,7 @@ return [
     */
 
     'token' => [
-        'songshenzong',
+        'lenovo',
         'shuaijiang',
     ],
 
@@ -84,7 +84,7 @@ return [
         'exceptions'      => true,  // Exception displayer
         'log'             => true,  // Logs from Monolog (merged in messages if enabled)
         'db'              => true,  // Show database (PDO) queries and bindings
-        'views'           => true,  // Views with their data
+        'views'           => false,  // Views with their data
         'route'           => true,  // Current route information
         'laravel'         => false, // Laravel version and environment
         'events'          => false, // All events fired
